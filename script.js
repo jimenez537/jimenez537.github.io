c8c93d4a92e2529bfaacebe6d668b0c5cca1f2a4
jimenez537 <89271263+jimenez537@users.noreply.github.com>
2025-01-16 14:08:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/HTTPdWmzl/";
+let URL = "https://teachablemachine.withgoogle.com/models/O488XWQZn/";
 
 
 
@@ -231,7 +231,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/HTTPdWmzl/");
+        console.log("https://teachablemachine.withgoogle.com/models/O488XWQZn/");
     }
 }
 
